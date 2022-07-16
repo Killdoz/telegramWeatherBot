1977b1bae2dc5d5f2ae89143960fb2967d4bb45d
Vladyslav <70052201+Killdoz@users.noreply.github.com>
2022-07-16 21:48:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import telebot 
+import json
+from config import token_weather, token_telega
+
+
+
+bot = telebot.TeleBot(token_telega)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    mess = f'Hi, {message.from_user.first_name}. I am the weather bot. Please, enter your city.'
+    bot.send_message(message.chat.id, mess)
+
+
+@bot.message_handler(content_types=["text"])
+def finish(message):
+    city = message.text
+    r = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={token_weather}&units=metric')
+    data = r.json()
+    with open('data.txt', 'w+') as outfile:
+        json.dump(r.json(), outfile)
+    temp = data['main']['temp']
+    bot.reply_to(message, f'Temperature in {city} is {temp}')
+
+
+
+
+
+bot.polling(none_stop=True)
